c9611ff0c1557973d11142c9fefc7e10a542c76d
Ismael Albert <albertismael7@gmail.com>
2021-09-06 14:44:53 -0300

    fin

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  color: #000;
+  height: 30vh;
+  background-color: ${(props) => props.theme.colors.primary};
+`;
---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,12 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  title: "dark",
+
+  colors: {
+    primary: "#333",
+    secundary: "#256",
+
+    background: "#222",
+    text: "#fff",
+  },
+};
